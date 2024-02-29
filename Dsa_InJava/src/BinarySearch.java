171d9cff98ad2707722af027df4d8c65d95c69c0
Mridul chourasia <99160348+mridulchourasiya@users.noreply.github.com>
2024-02-29 20:39:37 +0530

    day 01

---
+++ Dsa_InJava/src/BinarySearch.java
@@ -0,0 +1,29 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {-12, -11, -3, -1, 0, 2, 3, 4, 8, 19, 22, 44, 89};
+        int target = 22;
+        int ans = binarySearch(arr, target);
+        System.out.println(ans);
+    }
+
+    // return Index
+    // return -1 if it does not exist
+    static int binarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            // find the middle element
+            // int mid = (start + end) / 2; also possible exe ede the range in java
+            int mid = (end - start) / 2; // Issue: Incorrectly calculating the mid index
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
